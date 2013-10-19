453905b4a7c485b49398000577d27c758aee3d05
Tyler Collard <tcollard@gmail.com>
2013-10-19 12:18:00 +0200

    Updates to add numeral returns.

--- numerals.js
+++ numerals.js
@@ -1,8 +1,23 @@
-var nums = [1,4,5,9,10,40,50];
+var nums = [
+    { num: 1, numeral: 'I' },
+    { num: 4, numeral: 'IV' },
+    { num: 5, numeral: 'V' },
+    { num: 9, numeral: 'IX' },
+    { num: 10, numeral: 'X' },
+    { num: 40, numeral: 'XL' },
+    { num: 50, numeral: 'L' },
+    { num: 90, numeral: 'XC' },
+    { num: 100, numeral: 'C' },
+    { num: 400, numeral: 'CD' },
+    { num: 500, numeral: 'D' },
+    { num: 900, numeral: 'CM' },
+    { num: 1000, numeral: 'M' }
+];
+
 var findNextIndex = function(x, inc){
     var i = inc || 0;
-    if(nums[i] <= x){
-        return findNextIndex(x, i+1);
+    if(i < nums.length && getNumberFromIndex(i) <= x){
+        return findNextIndex(x, i + 1);
     }
 
     return inc - 1;
@@ -14,15 +29,31 @@ var sum = function(arr){
     }, 0);
 };
 
+var getNumberFromIndex = function(x){
+    return nums[x].num;
+};
+
+var getNumeralFromIndex = function(x){
+    return nums[x].numeral;
+};
+
+var getNumeralFromNumber = function(x){
+    for(var i = 0; i < nums.length; i++){
+        if(nums[i].num === x){
+            return nums[i].numeral;
+        }
+    }
+};
+
 var getIntegerArray = function(x){
     var f = [];
     var next = x;
-    var current = nums[findNextIndex(x)];
+    var current = getNumberFromIndex(findNextIndex(x));
     f.push(current);
 
     while(sum(f) < x){
         next -= current;
-        current = nums[findNextIndex(next)];
+        current = getNumberFromIndex(findNextIndex(next));
         f.push(current);
     }
 
@@ -30,5 +61,7 @@ var getIntegerArray = function(x){
 };
 
 exports.findNumeral = function(x){
-    return getIntegerArray(x);
+    return getIntegerArray(x).map(function(i){
+        return getNumeralFromNumber(i);
+    }).join('');
 };
--- test/test.js
+++ test/test.js
@@ -3,7 +3,19 @@ var numerals = require('../numerals');
 require('should');
 
 describe('findNumeral', function(){
-    describe('findNumeral with 1 should return I', function(){
-        
+    it('should return I when passing 1', function(){
+        numerals.findNumeral(1).should.equal('I');
+    });
+
+    it('should return V when passing 5', function(){
+        numerals.findNumeral(5).should.equal('V');
+    });
+
+    it('should return XL when passing 40', function(){
+        numerals.findNumeral(40).should.equal('XL');
+    });
+
+    it('should return MMXIII when passing 2013', function(){
+        numerals.findNumeral(2013).should.equal('MMXIII');
     });
 });
